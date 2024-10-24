f6b8864feaa3de01bacbf3aba9e780ac878d6fc2
Sai Rohan Reddy <rohanreddysai@gmail.com>
2024-10-24 21:20:22 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,11 @@
 let image = document.getElementsByTagName("img")[0]
-let next = document.querySelector(".next")
-let back = document.querySelector(".back")
 let name_ch = document.querySelector("#name")
 let photo_gallary = ["luffy.jpg", "zoro.jpg", "sanji.jpg", "Nami.jpg", "Robin.jpg", "Usopp.jpg", "Tony.jpg", "frank.jpg", "jim.jpg", "brook.jpg", "end1.jpg"]
 let names = ["Monkey D. Luffy", "Roronoa Zoro", "Vinsmoke Sanji", "Nami", "Nico Robin", "Usopp", "Tony Tony Chopper", "franky", "jinbei", "Brook", "THE END"]
 let img = photo_gallary
 let value = -1 //3
 function photonext() {
+    let next = document.querySelector(".next")
     next.addEventListener("click", (eobj) => {
         value++
         if (value < photo_gallary.length) {//0<3 1<3,2<3
@@ -29,6 +28,7 @@ function photonext() {
     })
 }
 function photoback() {
+    let back = document.querySelector(".back")
     back.addEventListener("click", (eobj) => {
         value--//1,0
         if (value >= 0 && value < photo_gallary.length) {
